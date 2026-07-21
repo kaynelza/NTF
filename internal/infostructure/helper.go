395369c02be8d483adc36d87b8cfd8a96142ae32
kaynelza <atsubel@icloud.com>
2026-07-22 01:55:31 +0300

    (NTF-3) create structures and constants

---
+++ internal/infostructure/helper.go
@@ -0,0 +1,27 @@
+package infostructure
+
+import "time"
+
+type Status string
+
+const (
+	STATUS_UNSPECIFIED Status = "UNSPECIFIED"
+	STATUS_PENDING     Status = "PENDING"
+	STATUS_SENDING     Status = "SENDING"
+	STATUS_SENT        Status = "SENT"
+	STATUS_CANCELLED   Status = "CANCELLED"
+	STATUS_DEAD        Status = "DEAD"
+)
+
+type Notification struct {
+	id         string
+	recipient  string
+	title      string
+	body       string
+	send_at    time.Time
+	status     Status
+	attempts   int32
+	last_error string
+	created_at time.Time
+	sent_at    time.Time
+}
